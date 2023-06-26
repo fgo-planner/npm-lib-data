6beb4cfb1794428ce7535fdf556e919f9770247a
Alvin Quach <alvinquach91@gmail.com>
2023-06-25 18:44:37 -0700

    added dto for creating and update plan group

---
+++ data-types/src/dto/CreatePlanGroup.type.ts
@@ -0,0 +1,4 @@
+import { OmitEntityWithTimestampsProps } from '../entity/OmitEntityWithTimestampsProps.type';
+import { PlanGroup } from '../planner/PlanGroup.type';
+
+export type CreatePlanGroup = OmitEntityWithTimestampsProps<PlanGroup>;
---
+++ data-types/src/dto/UpdatePlanGroup.type.ts
@@ -0,0 +1,4 @@
+import { OmitEntityTimestampsProps } from '../entity/OmitEntityTimestampsProps.type';
+import { PlanGroup } from '../planner/PlanGroup.type';
+
+export type UpdatePlanGroup = OmitEntityTimestampsProps<PlanGroup>;
